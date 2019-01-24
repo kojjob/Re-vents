55be139e1a0a338827c277138a496a9db2484e9c
Kojo Kwakye <kodeon6@gmail.com>
2019-01-24 10:50:34 +0000

    Add form to app

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Button } from "semantic-ui-react";
+import cuid from 'cuid'
 import EventList from "../EventList/EventList";
 import EventForm from "../EventForm/EventForm";
 
@@ -71,6 +72,15 @@ class EventDashboard extends Component {
       isOpen: false
     });
   };
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    })
+  }
 
   render() {
     return (
@@ -84,7 +94,9 @@ class EventDashboard extends Component {
             positive
             content="Create Event"
           />
-          {this.state.isOpen && <EventForm handleCancel={this.handleCancel} />}
+          {this.state.isOpen && (
+          <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />
+          )}
         </Grid.Column>
       </Grid>
     );
